781c7094a2f25a70b6d502508e328a9e6fa64302
Luke McConnell <lukermcconnell@gmail.com>
2022-09-17 15:17:54 +0900

    Spring Boot Init

---
+++ src/test/java/com/mcconnell/leaderkeeper/LeaderKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcconnell.leaderkeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaderKeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
